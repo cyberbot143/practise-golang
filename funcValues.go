0f0a518ddda62e9f2acb2a0c535a889f92fd97eb
cyberbot143 <krishnasai952@gmail.com>
2019-11-18 23:07:25 -0500

    added function closure

--- funcValues.go
+++ funcValues.go
@@ -13,6 +13,14 @@ func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 func FunctionValues() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
@@ -21,4 +29,12 @@ func FunctionValues() {
 
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
